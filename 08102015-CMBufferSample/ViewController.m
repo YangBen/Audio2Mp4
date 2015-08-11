b6ad6c3a09853b6ae1e2a00983f96d946f31f0a9
apexis <apexis@apexistekiiMac.local>
2015-08-11 19:52:09 +0800

    v1.0.100 Add action LinearPCM to Mp4;

--- 08102015-CMBufferSample/ViewController.m
+++ 08102015-CMBufferSample/ViewController.m
@@ -24,7 +24,142 @@
 }
 
 
-- (IBAction)actionPcm2Mp4:(id)sender {
+- (IBAction)actionLpcm2Mp4:(id)sender {
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Hello" ofType:@"pcm"];
+    NSLog(@"Mark0811: filePath %@", filePath);
+    //------------------------------------------------------------------------------------ Read Data
+    NSFileHandle *fileHandler = [NSFileHandle fileHandleForReadingAtPath:filePath];
+    
+    char *tmpBytes = malloc(32768);
+    memset(tmpBytes, 0, 32768);
+    NSData *tmpData = [NSData dataWithBytes:tmpBytes length:32768];
+    
+    //------------------------------------------------------------------------------------ Create CMBlockBufferRef
+    CMBlockBufferRef blockBR;
+    CMBlockBufferCreateEmpty(NULL, 0, kCMBlockBufferAssureMemoryNowFlag, &blockBR);
+    CMBlockBufferCreateWithMemoryBlock(NULL, tmpBytes, tmpData.length, NULL, NULL, 0, tmpData.length, kCMBlockBufferAssureMemoryNowFlag, &blockBR);
+    NSLog(@"Mark0811: Get block %@", blockBR);
+    
+    //------------------------------------------------------------------------------------ Create CMSampleBufferRef
+    CMSampleBufferRef sampleBR;
+    CMAudioFormatDescriptionRef afdr;
+    
+    AudioStreamBasicDescription asbd = {0};
+    asbd.mSampleRate = 44100;
+    asbd.mFormatID = kAudioFormatLinearPCM;
+    asbd.mFormatFlags = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
+    asbd.mBytesPerPacket = 4;
+    asbd.mFramesPerPacket = 1;
+    asbd.mBytesPerFrame = 4;
+    asbd.mChannelsPerFrame = 2;
+    asbd.mBitsPerChannel = 16;
+    
+    CMAudioFormatDescriptionCreate(kCFAllocatorDefault, &asbd, 0, NULL, 0, NULL, NULL, &afdr);
+    CMAudioSampleBufferCreateWithPacketDescriptions(kCFAllocatorDefault, blockBR, YES, NULL, NULL, afdr, 32768/4, CMTimeMake(0, 44100), NULL, &sampleBR);
+    NSLog(@"Mark0811: sampleBR %@", sampleBR);
+    
+    //------------------------------------------------------------------------------------ Gen Out.mp4
+    NSError *error;
+    NSURL *fileDir = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    NSURL *fileUrl2 = [fileDir URLByAppendingPathComponent:@"test2.mp4"];
+    NSLog(@"Mark0810: url2 %@", fileUrl2);
+    if ([[NSFileManager defaultManager] fileExistsAtPath:fileUrl2.path]) {
+        [[NSFileManager defaultManager] removeItemAtPath:fileUrl2.path error:&error];
+        NSLog(@"Mark0810: remove %@", error);
+    };
+    
+    error = nil;
+    AVAssetWriter *astW = [AVAssetWriter assetWriterWithURL:fileUrl2 fileType:AVFileTypeMPEG4 error:&error];
+    if (error) {
+        NSLog(@"Mark0810: error %@", error);
+        exit(2);
+    }
+    NSLog(@"Mark0810: astW %@", astW);
+    
+    //------------------------------------------------------------------------------------ Create Writer
+    AudioChannelLayout acl;
+    bzero(&acl, sizeof(acl));
+    acl.mChannelLayoutTag = kAudioChannelLayoutTag_Stereo;
+    
+    NSDictionary *outputSettings = @{AVFormatIDKey : [NSNumber numberWithUnsignedInt:kAudioFormatMPEG4AAC],
+                       AVSampleRateKey : [NSNumber numberWithFloat:44100.0],
+                       AVChannelLayoutKey : [NSData dataWithBytes:&acl length:sizeof(acl)]};
+    
+    AVAssetWriterInput *astWI = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeAudio outputSettings:outputSettings];
+    if (![astW canAddInput:astWI]) {
+        exit(4);
+    }
+    [astW addInput:astWI];
+    NSLog(@"Mark0810: inputs %@", astW.inputs);
+    
+    astWI.expectsMediaDataInRealTime = NO;
+    [astW startWriting];
+    [astW startSessionAtSourceTime:CMTimeMake(0, 44100)];
+    
+    //------------------------------------------------------------------------------------ Write process
+    BOOL isEnd = NO;
+    while (!isEnd) {
+        while (astWI.isReadyForMoreMediaData) {
+            tmpData = [fileHandler readDataOfLength:32768];
+            
+            if (!tmpData) {
+                [fileHandler closeFile];
+                isEnd = YES;
+                break;
+            }
+
+            CMBlockBufferReplaceDataBytes(tmpData.bytes, blockBR, 0, tmpData.length);
+            [astWI appendSampleBuffer:sampleBR];
+            
+            NSLog(@"Mark0811: Loop %lu", (unsigned long)tmpData.length);
+            if (tmpData.length < 32768) {
+                [fileHandler closeFile];
+                isEnd = YES;
+                break;
+            }
+            
+            usleep(2 * 1000);   // Test on iPhone4s, No need this sleep.
+            // if usleep(1 * 1000), or no sleep,  simulator will have duplicated audio played;
+        }
+        
+        usleep(100 * 1000);
+        NSLog(@"Mark0811: sleep");
+    }
+    
+    [astW finishWritingWithCompletionHandler:^{
+        NSLog(@"Mark0811: write finished.");
+    }];
+    
+    free(tmpBytes);
+    
+    //------------------------------------------------------------------------------------ Test Output file
+    _player = [[MPMoviePlayerController alloc] initWithContentURL:fileUrl2];
+    [_player prepareToPlay];
+    [_player.view setFrame:self.view.bounds];
+    [self.view addSubview:_player.view];
+    [_player play];
+    
+//////////////////////////////////////////////////////////////////////////////////////////   Just Read Code : For Beta Test
+//
+//    __block BOOL shouldKeepRunning = YES;
+//    [fileHandler setReadabilityHandler:^(NSFileHandle *fh) {
+//        NSData *tmpData = [fh readDataOfLength:32768];
+//        NSLog(@"Mark0811: Read Loop %lu", (unsigned long)tmpData.length);
+//        
+//        //-------------------------------------------------------------------------------- Create CMBlockBufferRef
+//        
+//        
+//        if (!tmpData || tmpData.length < 32768){
+//            [fh closeFile];
+//            shouldKeepRunning = NO;
+//            NSLog(@"Mark0811: Read End");
+//        }
+//        
+//    }];
+//    while (shouldKeepRunning && [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]]);
+//    NSLog(@"Mark0811: action End");
+//
+///////////////////////////////////////////////////////////////////////////////////////////
 }
 
 // Audio File convert to LinearPCM file
@@ -196,11 +331,16 @@
                     NSLog(@"Mark0810: appendErr %@", astW.error);
                     sleep(1);
                 }
-                NSLog(@"Mark: Loop");
+                NSLog(@"Mark0810: Loop");
+                
+                static dispatch_once_t onceToken;
+                dispatch_once(&onceToken, ^{
+                    NSLog(@"Mark0811: sampleBR %@", sampleBR);
+                });
             }
         }
         usleep(100*1000);
-        NSLog(@"Sleep");
+        NSLog(@"Mark0810: Sleep");
     }
     
     [astW finishWritingWithCompletionHandler:^{
